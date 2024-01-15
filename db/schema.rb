dda34eea37ab23222d6f1a45fdf56089612ab902
kosui3625 <154409416+kosui3625@users.noreply.github.com>
2024-01-15 14:35:23 +0900

    ログイン画面と新規登録画面の実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_12_081500) do
-  create_table "users", charset: "utf8", force: :cascade do |t|
+ActiveRecord::Schema[7.0].define(version: 2024_01_15_025246) do
+  create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "user", default: "", null: false
-    t.string "profile", default: "", null: false
-    t.string "occupation", default: "", null: false
-    t.string "position", default: "", null: false
+    t.string "name", null: false
+    t.text "profile", null: false
+    t.text "occupation", null: false
+    t.text "position", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
